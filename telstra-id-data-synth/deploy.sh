e629c6e09cee5eaff79420c652b0f0558a379f9c
Danny Tran <danny.tran@kasna.com.au>
2021-09-10 11:21:37 +1000

    Move datasynth to blueprints repo

--- telstra-id-data-synth/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker build -t datasynth .
-docker tag datasynth gcr.io/telstra-id-data-synth/datasynth
-docker push gcr.io/telstra-id-data-synth/datasynth
-
-cd terraform
-terrafrom init
-terraform apply --auto-approve
--- telstra-id-data-synth/python/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python3 ./python/australian-cdr-generator.py & python3 ./python/international-cdr-generator.py & python3 ./python/alarm-generator.py
