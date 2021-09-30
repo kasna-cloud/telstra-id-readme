cb31da8902cbdf9c022a93a68983f61991e90cbc
Stephen Bancroft <stephen.bancroft@kasna.com.au>
2021-09-30 12:02:15 +1000

    update project_id

--- telstra-id-data-synth/deploy.sh
+++ telstra-id-data-synth/deploy.sh
@@ -4,6 +4,7 @@ docker build -t datasynth .
 docker tag datasynth gcr.io/$PROJECT_ID/datasynth
 docker push gcr.io/$PROJECT_ID/datasynth
 
+export TF_VAR_project_id=$PROJECT_ID
 cd terraform
 terraform init
 terraform apply --auto-approve
