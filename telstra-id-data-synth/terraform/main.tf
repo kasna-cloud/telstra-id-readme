d8850542e402b24b211451b52ffee7cc2c774884
Stephen Bancroft <stephen.bancroft@kasna.com.au>
2021-10-01 12:27:51 +1000

    added subnetwork

--- telstra-id-data-synth/terraform/main.tf
+++ telstra-id-data-synth/terraform/main.tf
@@ -54,7 +54,7 @@ resource "google_compute_instance" "datasynth" {
   }
 
   network_interface {
-    network = google_compute_network.synth_network.name
+    subnetwork = google_compute_subnetwork.synth_subnetwork.name
   }
 
   metadata = {
@@ -83,7 +83,7 @@ resource "google_compute_network" "synth_network" {
   name                    = var.vpc-network
   auto_create_subnetworks = false
 }
-resource "google_compute_subnetwork" "synth_network" {
+resource "google_compute_subnetwork" "synth_subnetwork" {
   name          = "synth-subnetwork"
   ip_cidr_range = "10.152.0.0/24"
   region        = "australia-southeast1"
